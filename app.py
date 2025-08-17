68528ac1d36cb7ccc9ec2b2afe351ffc9e148bc8
Cleverson <clevercleverpassos@gmail.com>
2025-08-17 13:36:53 -0300

    Troca de nome de pasta origem para caminho origem

--- app.py
+++ app.py
@@ -17,24 +17,24 @@ def tarefa():
     }
 
     # Definir a pasta onde estão os arquivos desorganizados
-    pasta_origem = r"C:\Users\cleve\Downloads"
+    caminho_origem = r"C:\Users\cleve\Downloads"
 
     # Criar as subpastas para organização
     for pasta in categorias.keys():
-        caminho_pasta = os.path.join(pasta_origem, pasta)
+        caminho_pasta = os.path.join(caminho_origem, pasta)
         if not os.path.exists(caminho_pasta):
             os.mkdir(caminho_pasta)
 
     # Organizar os arquivos
-    for arquivo in os.listdir(pasta_origem):
-        caminho_arquivo = os.path.join(pasta_origem, arquivo)
+    for arquivo in os.listdir(caminho_origem):
+        caminho_arquivo = os.path.join(caminho_origem, arquivo)
         # Verificar se é um arquivo (e não um diretório)
         if os.path.isfile(caminho_arquivo):
             extensao = os.path.splitext(arquivo)[1]
             # Mover para a pasta correspondente com base na extensão
             for pasta, extensoes in categorias.items():
                 if extensao.lower() in extensoes:
-                    destino = os.path.join(pasta_origem, pasta, arquivo)
+                    destino = os.path.join(caminho_origem, pasta, arquivo)
                     shutil.move(caminho_arquivo, destino)
                     print(f"Movido: {arquivo} → {pasta}")
                     break
